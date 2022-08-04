907a43ec3ab45162790a3b9c37319bd103a4f88c
Software on Python <borodenko22448811@gmail.com>
2022-08-04 05:09:21 +0300

    Added view_site_user_profile page

--- exg1o/main/urls.py
+++ exg1o/main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from main.views import *
 
 urlpatterns = [
-	path('', main),
+	path('', main_page),
+	path('view_site_user_profile/<int:other_user_id>/', view_site_user_profile_page)
 ]
--- exg1o/main/views.py
+++ exg1o/main/views.py
@@ -1,9 +1,28 @@
 from django.core.handlers.wsgi import WSGIRequest
+from django.http import Http404
 from django.shortcuts import render
 from django.contrib.auth.models import User
 import global_functions as GlobalFunctions
 
 # Create your views here.
+def main_page(request: WSGIRequest): # Отрисовка main.html
+	site_users = []
+	for user in User.objects.all():
+		site_users.append(
+			{
+				'id': user.id,
+				'username': user.username
+			}
+		)
+
+	data = GlobalFunctions.get_navbar_buttons_data(request)
+	data.update(
+		{
+			'users': site_users
+		}
+	)
+	return render(request, 'main.html', data)
+
 def main(request: WSGIRequest): # Отрисовка main.html
 	site_users = []
 	for user in User.objects.all():
@@ -21,3 +40,24 @@ def main(request: WSGIRequest): # Отрисовка main.html
 		}
 	)
 	return render(request, 'main.html', data)
+
+def view_site_user_profile_page(request: WSGIRequest, other_user_id: int): # Отрисовка view_site_user_profile.html
+	other_user = User.objects.filter(id=other_user_id)
+
+	if other_user.exists():
+		other_user = other_user[0]
+
+		data = GlobalFunctions.get_navbar_buttons_data(request)
+		data.update(
+			{
+				'other_user': {
+					'id': other_user.id,
+					'username': other_user.username,
+					'status': 'Бесплатный' if other_user.groups.get().name == 'free_accounts' else 'Платный',
+					'date_joined': other_user.date_joined
+				}
+			}
+		)
+		return render(request, 'view_site_user_profile.html', data)
+	else:
+		raise Http404(f'Пользователя сайта под ID {other_user_id} не существует!')
