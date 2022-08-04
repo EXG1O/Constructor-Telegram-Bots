587f2bfc1efc465b948819829f0ed68a611eac58
Software on Python <borodenko22448811@gmail.com>
2022-08-04 04:56:51 +0300

    Added site users list widget for main page

--- exg1o/main/views.py
+++ exg1o/main/views.py
@@ -1,8 +1,23 @@
 from django.core.handlers.wsgi import WSGIRequest
 from django.shortcuts import render
+from django.contrib.auth.models import User
 import global_functions as GlobalFunctions
 
 # Create your views here.
 def main(request: WSGIRequest): # Отрисовка main.html
+	site_users = []
+	for user in User.objects.all():
+		site_users.append(
+			{
+				'id': user.id,
+				'username': user.username
+			}
+		)
+
 	data = GlobalFunctions.get_navbar_buttons_data(request)
+	data.update(
+		{
+			'users': site_users
+		}
+	)
 	return render(request, 'main.html', data)
