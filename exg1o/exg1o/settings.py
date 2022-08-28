766bd097e141dc0a298e2cc994f4cd74cf1afa53
EXG1O <borodenko22448811@gmail.com>
2022-08-28 21:35:09 +0300

    Added STATIC_ROOT

--- exg1o/exg1o/settings.py
+++ exg1o/exg1o/settings.py
@@ -221,7 +221,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
