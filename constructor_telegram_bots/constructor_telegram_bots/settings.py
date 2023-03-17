874757cb14de51793837164cd39861bb3970b1a9
EXG1O <borodenko22448811@gmail.com>
2023-03-17 18:46:00 +0200

    DEBUG=True

--- constructor_telegram_bots/constructor_telegram_bots/settings.py
+++ constructor_telegram_bots/constructor_telegram_bots/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
