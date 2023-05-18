b6b705d012a50c38125b949223ee9b03ca4571c6
EXG1O <borodenko22448811@gmail.com>
2023-05-18 23:48:52 +0300

    Updated /donation/ page and add /donation/completed/ page

--- constructor_telegram_bots/donation/views.py
+++ constructor_telegram_bots/donation/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import HttpResponse, render
 
 def donation(request: WSGIRequest) -> HttpResponse:
 	return render(request, 'donation.html')
+
+def donation_completed(request: WSGIRequest) -> HttpResponse:
+	return render(request, 'donation_completed.html')
