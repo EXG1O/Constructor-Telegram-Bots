bfd0cdb4fde0b70605cef71679dd2d1ed19b523f
EXG1O <borodenko22448811@gmail.com>
2023-06-18 16:50:50 +0300

    Added JS script for show user messages

--- constructor_telegram_bots/static/js/global/show_user_messages.js
+++ constructor_telegram_bots/static/js/global/show_user_messages.js
@@ -1,11 +1,15 @@
-fetch(getUserMessagesUrl, {
-	method: 'POST'
-}).then(response => {
-	if (response.ok) {
-		response.json().then(messages => {
-			messages.forEach(message => {
-				createAlert(mainAlertContainer, message['text'], message['type']);
+function showUserMessages() {
+	fetch(getUserMessagesUrl, {
+		method: 'POST'
+	}).then(response => {
+		if (response.ok) {
+			response.json().then(messages => {
+				messages.forEach(message => {
+					createAlert(mainAlertContainer, message['text'], message['type']);
+				});
 			});
-		});
-	}
-});
+		}
+	});
+}
+
+setInterval(showUserMessages, 2500);
