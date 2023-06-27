4749307ee09b9ba48fed721869fdef0648345c0a
EXG1O <borodenko22448811@gmail.com>
2023-06-27 21:43:28 +0300

    Updated alert.js

--- constructor_telegram_bots/static/js/global/alert.js
+++ constructor_telegram_bots/static/js/global/alert.js
@@ -4,45 +4,36 @@
 
 	var mainAlertContainer = document.querySelector('#mainAlertContainer');
 
-	const breakpoints = ['', 'sm-', 'md-', 'lg-', 'xl-', 'xxl-'];
+	const bootstrapBreakpoints = ['', 'sm-', 'md-', 'lg-', 'xl-', 'xxl-'];
+
+	const alertIcons = {
+		'primary': '<i class="bi bi-info-circle-fill me-2"></i>',
+		'success': '<i class="bi bi-check-circle-fill me-2"></i>',
+		'danger': '<i class="bi bi-exclamation-triangle-fill me-2"></i>',
+		'warning': '<i class="bi bi-exclamation-triangle-fill me-2"></i>',
+	}
 
 	let alertId = 0;
 
-	function removeAlert(alertDiv) {
-		alertDiv.remove();
-
-		if (alertDiv.id == alertId - 1) {
-			mainContainer.setAttribute('class', mainContainerClass);
-		}
-	}
-
-	function closeAlert(alertDiv) {
-		alertDiv.classList.remove('show');
-
-		setTimeout(removeAlert, 300, alertDiv);
-	}
-
-	function showAlert(alertDiv) {
-		alertDiv.classList.add('show');
-
-		setTimeout(closeAlert, 5000, alertDiv);
-	}
-
 	function createAlert(alertContainer, message, type) {
-		let alertDiv = document.createElement('div');
-		alertDiv.className = `alert alert-${type} fade `;
+		const alertDiv = document.createElement('div');
+		alertDiv.className = `alert alert-${type} d-flex align-items-center alert-dismissible fade `;
 		alertDiv.id = alertId;
 		alertDiv.role = 'alert';
-		alertDiv.innerHTML = `<p class="text-center mb-0">${message}</p>`;
+		alertDiv.innerHTML = [
+			alertIcons[type],
+			`<p class="mb-0">${message}</p>`,
+			'<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+		].join('');
 
 		if (alertContainer == mainAlertContainer) {
-			for (let breakpoint = 0; breakpoint < breakpoints.length; breakpoint++) {
+			for (let bootstrapBreakpoint = 0; bootstrapBreakpoint < bootstrapBreakpoints.length; bootstrapBreakpoint++) {
 				for (let size = 1; size <= 5; size++) {
-					if (mainContainer.classList.contains(`my-${breakpoints[breakpoint]}${size}`)) {
-						mainContainer.classList.remove(`my-${breakpoints[breakpoint]}${size}`);
-						mainContainer.classList.add(`mb-${breakpoints[breakpoint]}${size}`);
-					} else if (mainContainer.classList.contains(`mt-${breakpoints[breakpoint]}${size}`)) {
-						mainContainer.classList.remove(`mt-${breakpoints[breakpoint]}${size}`);
+					if (mainContainer.classList.contains(`my-${bootstrapBreakpoints[bootstrapBreakpoint]}${size}`)) {
+						mainContainer.classList.remove(`my-${bootstrapBreakpoints[bootstrapBreakpoint]}${size}`);
+						mainContainer.classList.add(`mb-${bootstrapBreakpoints[bootstrapBreakpoint]}${size}`);
+					} else if (mainContainer.classList.contains(`mt-${bootstrapBreakpoints[bootstrapBreakpoint]}${size}`)) {
+						mainContainer.classList.remove(`mt-${bootstrapBreakpoints[bootstrapBreakpoint]}${size}`);
 					}
 				}
 			}
@@ -54,7 +45,21 @@
 
 		alertContainer.append(alertDiv);
 
-		setTimeout(showAlert, 200, alertDiv);
+		setTimeout((alertDiv) => {
+			alertDiv.classList.add('show');
+			
+			setTimeout((alertDiv) => {
+				alertDiv.classList.remove('show');
+				
+				setTimeout((alertDiv) => {
+					alertDiv.remove();
+
+					if (alertDiv.id == alertId - 1) {
+						mainContainer.setAttribute('class', mainContainerClass);
+					}
+				}, 300, alertDiv);
+			}, 5000, alertDiv);
+		}, 250, alertDiv);
 
 		alertId ++;
 	}
