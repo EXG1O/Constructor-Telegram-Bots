d5af79f4189a2ded46e4e0da6274852dd65482fa
EXG1O <borodenko22448811@gmail.com>
2023-07-02 00:02:56 +0300

    Writed tests for instruction Django app

--- constructor_telegram_bots/instruction/tests.py
+++ constructor_telegram_bots/instruction/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.http import HttpResponse
+from django import urls
 
-# Create your tests here.
+
+class InstructionViewsTests(TestCase):
+	def setUp(self) -> None:
+		self.client = Client(enforce_csrf_checks=True)
+
+	def test_instruction_view(self) -> None:
+		response: HttpResponse = self.client.get(urls.reverse('instruction'))
+		self.assertEqual(response.status_code, 200)
+		self.assertTemplateUsed(response, 'instruction.html')
