b534516865413e08df42937a337645fafac5c698
EXG1O <borodenko22448811@gmail.com>
2023-07-01 04:47:59 +0300

    Updated 'Updates' page

--- constructor_telegram_bots/updates/admin.py
+++ constructor_telegram_bots/updates/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from modeltranslation.admin import TranslationAdmin
+from django.utils.translation import gettext_lazy as _
 
 from ckeditor.widgets import CKEditorWidget
 
@@ -9,7 +10,16 @@ from updates.models import Update
 
 @admin.register(Update)
 class UpdateAdmin(TranslationAdmin):
-	list_display = ('title', '_date_added')
+	list_display = ('title', 'views', 'show_comments_number', 'show_likes_number','date_added')
 
 	fields = ('image', 'title', 'description')
 	formfield_overrides = {models.TextField: {'widget': CKEditorWidget}}
+
+	@admin.display(description=_('Количество комментариев'))
+	def show_comments_number(self, update: Update) -> int:
+		return update.comments.count()
+
+	@admin.display(description=_('Количество лайков'))
+	def show_likes_number(self, update: Update) -> int:
+		print(update)
+		return update.likes.count()
--- constructor_telegram_bots/updates/models.py
+++ constructor_telegram_bots/updates/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from django.conf import settings
 
-import pytz
+from user.models import User
 
 
 class Update(models.Model):
 	image = models.ImageField(upload_to='static/images/updates/')
 	title = models.CharField(_('Заголовок'), max_length=255)
 	description = models.TextField(_('Описание'))
-	_date_added = models.DateTimeField(_('Дата добавления'), auto_now_add=True)
+	views = models.BigIntegerField(_('Просмотры'), default=0)
+	date_added = models.DateTimeField(_('Дата добавления'), auto_now_add=True)
 
 	class Meta:
 		db_table = 'update'
@@ -17,8 +17,19 @@ class Update(models.Model):
 		verbose_name = _('Обновление')
 		verbose_name_plural = _('Обновления')
 
-	@property
-	def date_added(self) -> str:
-		return self._date_added.astimezone(
-			pytz.timezone(settings.TIME_ZONE)
-		).strftime('%d.%m.%Y - %H:%M:%S')
+class UpdateComment(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	update = models.ForeignKey(Update, on_delete=models.CASCADE, related_name='comments')
+	text = models.TextField()
+	date_added = models.DateTimeField(auto_now_add=True)
+
+	class Meta:
+		db_table = 'update_comment'
+
+class UpdateLike(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	update = models.ForeignKey(Update, on_delete=models.CASCADE, related_name='likes')
+	date_added = models.DateTimeField(auto_now_add=True)
+
+	class Meta:
+		db_table = 'update_like'
