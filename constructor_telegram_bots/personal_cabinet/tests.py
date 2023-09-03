3ee18e16a826597c2186e0efb038bb58cf885a7d
EXG1O <borodenko22448811@gmail.com>
2023-09-04 00:17:30 +0300

    Refactored personal_cabinet app

--- constructor_telegram_bots/personal_cabinet/tests.py
+++ constructor_telegram_bots/personal_cabinet/tests.py
@@ -1,15 +1,21 @@
-from constructor_telegram_bots.tests import BaseTestCase
-
+from django.test import TestCase, Client
 from django.http import HttpResponse
 from django import urls
 
+from user.models import User
 
-class PersonalCabinetViewsTests(BaseTestCase):
+
+class ViewsTests(TestCase):
+	def setUp(self) -> None:
+		self.client = Client(enforce_csrf_checks=True)
+		self.user: User = User.objects.create(telegram_id=123456789, first_name='exg1o')
+
 	def test_personal_cabinet_view(self) -> None:
 		url: str = urls.reverse('personal_cabinet')
 
 		response: HttpResponse = self.client.get(url)
 		self.assertEqual(response.status_code, 302)
+
 		self.client.get(self.user.login_url)
 
 		response: HttpResponse = self.client.get(url)
