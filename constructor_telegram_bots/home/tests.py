942d607699cb8661aa7b0df698ad6ed92bbcd33d
EXG1O <borodenko22448811@gmail.com>
2023-09-04 00:03:38 +0300

    Refactored home app

--- constructor_telegram_bots/home/tests.py
+++ constructor_telegram_bots/home/tests.py
@@ -1,12 +1,9 @@
-from django.test import TestCase, Client
+from django.test import TestCase
 from django.http import HttpResponse
 from django import urls
 
 
-class HomeViewsTests(TestCase):
-	def setUp(self) -> None:
-		self.client = Client()
-
+class ViewsTests(TestCase):
 	def test_home_view(self) -> None:
 		url: str = urls.reverse('home')
 		response: HttpResponse = self.client.get(url)
