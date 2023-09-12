6e36ef646cb3135c50ad61443abf6fb3acb223ce
EXG1O <borodenko22448811@gmail.com>
2023-09-12 23:46:49 +0300

    Fixed bug for user local PC

--- constructor_telegram_bots/constructor_telegram_bots/environment.py
+++ constructor_telegram_bots/constructor_telegram_bots/environment.py
@@ -2,49 +2,62 @@ from django.conf import settings
 
 from asgiref.sync import sync_to_async
 import requests
+import jinja2
 
 
 def create_user(user) -> None:
-	if not settings.TEST:
-		requests.post('http://127.0.0.1:99/users/', json={'user_id': user.id, 'token': user.auth_token.key})
+	if settings.DEBUG or settings.TEST:
+		return
+
+	requests.post('http://127.0.0.1:99/users/', json={'user_id': user.id, 'token': user.auth_token.key})
 
 def delete_user(user) -> None:
-	if not settings.TEST:
-		requests.delete(f'http://127.0.0.1:99/users/{user.id}/')
+	if settings.DEBUG or settings.TEST:
+		return
+
+	requests.delete(f'http://127.0.0.1:99/users/{user.id}/')
 
 def add_plugin(plugin) -> None:
-	if not settings.TEST:
-		requests.post('http://127.0.0.1:99/plugins/', json={
-			'user_id': plugin.user.id,
-			'token': plugin.user.auth_token.key,
-			'telegram_bot_id': plugin.telegram_bot.id,
-			'plugin_id': plugin.id,
-			'name': plugin.name,
-			'code': plugin.code,
-		})
+	if settings.DEBUG or settings.TEST:
+		return
+
+	requests.post('http://127.0.0.1:99/plugins/', json={
+		'user_id': plugin.user.id,
+		'token': plugin.user.auth_token.key,
+		'telegram_bot_id': plugin.telegram_bot.id,
+		'plugin_id': plugin.id,
+		'name': plugin.name,
+		'code': plugin.code,
+	})
 
 def update_plugin(plugin) -> None:
-	if not settings.TEST:
-		requests.post('http://127.0.0.1:99/plugins/', json={
-			'user_id': plugin.user.id,
-			'token': plugin.user.auth_token.key,
-			'telegram_bot_id': plugin.telegram_bot.id,
-			'plugin_id': plugin.id,
-			'name': plugin.name,
-			'code': plugin.code,
-		})
+	if settings.DEBUG or settings.TEST:
+		return
+
+	requests.post('http://127.0.0.1:99/plugins/', json={
+		'user_id': plugin.user.id,
+		'token': plugin.user.auth_token.key,
+		'telegram_bot_id': plugin.telegram_bot.id,
+		'plugin_id': plugin.id,
+		'name': plugin.name,
+		'code': plugin.code,
+	})
 
 def delete_plugin(plugin) -> None:
-	if not settings.TEST:
-		requests.delete('http://127.0.0.1:99/plugins/', json={
-			'user_id': plugin.user.id,
-			'telegram_bot_id': plugin.telegram_bot.id,
-			'name': plugin.name,
-		})
+	if settings.DEBUG or settings.TEST:
+		return
+
+	requests.delete('http://127.0.0.1:99/plugins/', json={
+		'user_id': plugin.user.id,
+		'telegram_bot_id': plugin.telegram_bot.id,
+		'name': plugin.name,
+	})
 
 def replace_text_variables(telegram_bot, text: str, text_variables: dict) -> str:
-	if settings.TEST:
-		return text
+	if settings.DEBUG or settings.TEST:
+		environment = jinja2.Environment()
+		template: jinja2.Template = environment.from_string(text)
+		return template.render(text_variables)
 
 	return requests.post('http://127.0.0.1:99/generate/template/', json={
 		'user_id': telegram_bot.owner.id,
