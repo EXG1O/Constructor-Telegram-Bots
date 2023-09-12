8efe33d5b5e04b4f7b7401ec0758292f61caa369
EXG1O <borodenko22448811@gmail.com>
2023-09-13 00:15:15 +0300

    Updated decorator for plugin app

--- constructor_telegram_bots/plugin/decorators.py
+++ constructor_telegram_bots/plugin/decorators.py
@@ -2,9 +2,11 @@ from django.utils.translation import gettext as _
 
 from rest_framework.response import Response
 
-from .models import Plugin
 from telegram_bot.models import TelegramBot
 
+from .models import Plugin
+
+from typing import Optional
 from functools import wraps
 import re
 
@@ -26,47 +28,34 @@ def check_plugin_id(func):
 def check_plugin_data(func):
 	@wraps(func)
 	def wrapper(*args, **kwargs):
+		telegram_bot: TelegramBot = kwargs['telegram_bot'] if 'telegram_bot' in kwargs else kwargs['plugin'].telegram_bot
+		name: Optional[str] = kwargs['name'] if 'name' in kwargs else None
 		code: str = kwargs['code']
 
-		if 'name' in kwargs:
-			name: str = kwargs['name']
-
+		if name is not None:
 			if name:
-				if len(name) > 255:
-					return Response({
-						'message': _('Название плагина должно содержать не более 255 символов!'),
-						'level': 'danger',
-					}, status=400)
-
-				if re.match(r'^[a-zA-Z_][a-zA-Z0-9_]*$', name) is None:
-					return Response({
-						'message': _('Название плагина содержит запрещенные символы!'),
-						'level': 'danger',
-					}, status=400)
-
-				telegram_bot = None
-
-				if 'telegram_bot' in kwargs:
-					telegram_bot: TelegramBot = kwargs['telegram_bot']
-				elif 'plugin' in kwargs:
-					telegram_bot: TelegramBot =  kwargs['plugin'].telegram_bot
-
-				if not telegram_bot:
-					return Response({
-						'message': _('Произошла ошибка, попробуйте позже!'),
-						'level': 'danger',
-					}, status=500)
-
-				if Plugin.objects.filter(telegram_bot=telegram_bot, name=name).exists():
-					return Response({
-						'message': _('У вас уже добавлен плагин с таким названием!'),
-						'level': 'danger',
-					}, status=400)
-			else:
 				return Response({
 					'message': _('Введите название плагина!'),
 					'level': 'danger',
 				}, status=400)
+
+			if len(name) > 255:
+				return Response({
+					'message': _('Название плагина должно содержать не более 255 символов!'),
+					'level': 'danger',
+				}, status=400)
+
+			if re.match(r'^[a-zA-Z_][a-zA-Z0-9_]*$', name) is None:
+				return Response({
+					'message': _('Название плагина содержит запрещенные символы!'),
+					'level': 'danger',
+				}, status=400)
+
+		if Plugin.objects.filter(telegram_bot=telegram_bot, name=name).exists():
+			return Response({
+				'message': _('У вас уже добавлен плагин с таким названием!'),
+				'level': 'danger',
+			}, status=400)
 
 		if not code:
 			return Response({
