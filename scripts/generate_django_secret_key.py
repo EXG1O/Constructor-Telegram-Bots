444b614bd0cf0d46f98c428abf422440bbd78dd1
EXG1O <borodenko22448811@gmail.com>
2023-11-08 02:10:09 +0200

    Moved all folders up one level

--- scripts/generate_django_secret_key.py
+++
@@ -1,9 +0,0 @@
-import random
-
-
-random_string = ''
-
-for num in range(50):
-	random_string += random.choice('abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_')
-
-print(f'django-insecure-{random_string}')
--- setup.sh
+++ setup.sh
@@ -6,15 +6,13 @@ source env/bin/activate
 pip install -U pip
 pip install -r requirements.txt
 
-SECRET_KEY=$(python scripts/generate_django_secret_key.py)
+SECRET_KEY=$(python -c "import random, string; print(f\"django-insecure-{''.join([random.choice(string.ascii_letters + string.digits) for num in range(50)])}\")")
 
 read -p "Enter Telegram bot username: " TELEGRAM_BOT_USERNAME
 read -p "Enter Telegram bot API-Token: " TELEGRAM_BOT_TOKEN
 read -p "Enter PostgreSQL database name: " POSTGRESQL_DATABASE_NAME
 read -p "Enter PostgreSQL database user: " POSTGRESQL_DATABASE_USER
 read -p "Enter PostgreSQL database password: " POSTGRESQL_DATABASE_PASSWORD
-
-cd constructor_telegram_bots
 
 cat << EOF > .env
 SECRET_KEY='$SECRET_KEY'
@@ -29,5 +27,5 @@ POSTGRESQL_DATABASE_USER=$POSTGRESQL_DATABASE_USER
 POSTGRESQL_DATABASE_PASSWORD=$POSTGRESQL_DATABASE_PASSWORD
 EOF
 
-python manage.py compilemessages
+python manage.py compilemessages -i env
 python manage.py migrate
