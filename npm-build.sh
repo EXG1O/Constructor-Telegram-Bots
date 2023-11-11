5723f164708f63d40b71b85c4441e784d7e56781
EXG1O <borodenko22448811@gmail.com>
2023-11-11 07:36:24 +0200

    Fixed action bug of django.yml

--- npm-build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-WEBPACK_CONFIGS=('./constructor_telegram_bots/static/global/webpack.config.js')
-
-for WEBPACK_CONFIG in $WEBPACK_CONFIGS; do
-	node_modules/.bin/webpack --mode=production -c $WEBPACK_CONFIG
-done
