04b4d28d471e89d1fd9332bb14e6265f6fcb7883
EXG1O <borodenko22448811@gmail.com>
2023-12-05 02:07:43 +0200

    Added jsi18n and make for him .d.ts

--- constructor_telegram_bots/urls.py
+++ constructor_telegram_bots/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from django.contrib import admin
+from django.views.i18n import JavaScriptCatalog
 from django.conf import settings
 
 import sys
@@ -8,6 +9,7 @@ import sys
 urlpatterns = [
 	path('admin/', admin.site.urls),
 	path('i18n/', include('django.conf.urls.i18n')),
+	path('jsi18n/', JavaScriptCatalog.as_view(), name='javascript-catalog'),
 
 	path('tinymce/', include('tinymce.urls')),
 
