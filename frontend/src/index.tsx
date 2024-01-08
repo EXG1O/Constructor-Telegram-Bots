4d305cfe1815af090adb21bc8222626675305c64
EXG1O <borodenko22448811@gmail.com>
2024-01-08 19:16:13 +0200

    Deleted ErrorBoundary frontend router

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -23,129 +23,120 @@ const router = createBrowserRouter([
 		shouldRevalidate: () => true,
 		children: [
 			{
+				id: 'home',
+				index: true,
 				async lazy() {
-					const module = await import('./routes/ErrorBoundary');
+					const module = await import('./routes/Home');
 
-					return { ErrorBoundary: module.default };
+					return {
+						Component: module.default,
+						loader: module.loader,
+					}
+				},
+			},
+			{
+				id: 'team',
+				path: 'team/',
+				async lazy() {
+					const module = await import('./routes/Team');
+
+					return {
+						Component: module.default,
+						loader: module.loader,
+					}
+				},
+			},
+			{
+				path: 'donation/',
+				children: [
+					{
+						id: 'donation-index',
+						index: true,
+						async lazy() {
+							const module = await import('./routes/Donation/Index');
+
+							return {
+								Component: module.default,
+								loader: module.loader,
+							}
+						},
+					},
+					{
+						path: 'completed/',
+						async lazy() {
+							const module = await import('./routes/Donation/Completed');
+
+							return { Component: module.default };
+						},
+					},
+				],
+			},
+			{
+				id: 'instruction',
+				path: 'instruction/',
+				async lazy() {
+					const module = await import('./routes/Instruction');
+
+					return {
+						Component: module.default,
+						loader: module.loader,
+					}
+				},
+			},
+			{
+				async lazy() {
+					const module = await import('./routes/AuthRequired/Root');
+
+					return { Component: module.default };
 				},
 				children: [
 					{
-						id: 'home',
-						index: true,
+						path: 'personal-cabinet/',
 						async lazy() {
-							const module = await import('./routes/Home');
+							const module = await import('./routes/AuthRequired/PersonalCabinet');
 
-							return {
-								Component: module.default,
-								loader: module.loader,
-							}
+							return { Component: module.default };
 						},
 					},
 					{
-						id: 'team',
-						path: 'team/',
+						path: 'telegram-bot-menu/:telegramBotID/',
 						async lazy() {
-							const module = await import('./routes/Team');
-
-							return {
-								Component: module.default,
-								loader: module.loader,
-							}
-						},
-					},
-					{
-						path: 'donation/',
-						children: [
-							{
-								id: 'donation-index',
-								index: true,
-								async lazy() {
-									const module = await import('./routes/Donation/Index');
-
-									return {
-										Component: module.default,
-										loader: module.loader,
-									}
-								},
-							},
-							{
-								path: 'completed/',
-								async lazy() {
-									const module = await import('./routes/Donation/Completed');
-
-									return { Component: module.default };
-								},
-							},
-						],
-					},
-					{
-						id: 'instruction',
-						path: 'instruction/',
-						async lazy() {
-							const module = await import('./routes/Instruction');
-
-							return {
-								Component: module.default,
-								loader: module.loader,
-							}
-						},
-					},
-					{
-						async lazy() {
-							const module = await import('./routes/AuthRequired/Root');
+							const module = await import('./routes/AuthRequired/TelegramBotMenu/Root');
 
 							return { Component: module.default };
 						},
 						children: [
 							{
-								path: 'personal-cabinet/',
+								index: true,
 								async lazy() {
-									const module = await import('./routes/AuthRequired/PersonalCabinet');
+									const module = await import('./routes/AuthRequired/TelegramBotMenu/Index');
 
 									return { Component: module.default };
 								},
 							},
 							{
-								path: 'telegram-bot-menu/:telegramBotID/',
+								path: 'variables/',
 								async lazy() {
-									const module = await import('./routes/AuthRequired/TelegramBotMenu/Root');
+									const module = await import('./routes/AuthRequired/TelegramBotMenu/Variables');
 
 									return { Component: module.default };
 								},
-								children: [
-									{
-										index: true,
-										async lazy() {
-											const module = await import('./routes/AuthRequired/TelegramBotMenu/Index');
+							},
+							{
+								path: 'users/',
+								async lazy() {
+									const module = await import('./routes/AuthRequired/TelegramBotMenu/Users');
 
-											return { Component: module.default };
-										},
-									},
-									{
-										path: 'variables/',
-										async lazy() {
-											const module = await import('./routes/AuthRequired/TelegramBotMenu/Variables');
+									return { Component: module.default };
+								},
+							},
+							{
+								path: 'constructor/',
+								async lazy() {
+									const module = await import('./routes/AuthRequired/TelegramBotMenu/Constructor');
 
-											return { Component: module.default };
-										},
-									},
-									{
-										path: 'users/',
-										async lazy() {
-											const module = await import('./routes/AuthRequired/TelegramBotMenu/Users');
-
-											return { Component: module.default };
-										},
-									},
-									{
-										path: 'constructor/',
-										async lazy() {
-											const module = await import('./routes/AuthRequired/TelegramBotMenu/Constructor');
-
-											return { Component: module.default };
-										},
-									},
-								],
+									return { Component: module.default };
+								},
 							},
 						],
 					},
