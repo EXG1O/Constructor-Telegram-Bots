9fefe606d1a4bc307ba040b3f213d24a9e8b8277
EXG1O <borodenko22448811@gmail.com>
2024-01-22 16:01:33 +0200

    Added PrivacyPolicySection model of privacy_policy app to admin

--- privacy_policy/admin.py
+++ privacy_policy/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.db import models
 
-# Register your models here.
+from modeltranslation.admin import TranslationAdmin
+from tinymce.widgets import TinyMCE
+
+from .models import PrivacyPolicySection
+
+
+@admin.register(PrivacyPolicySection)
+class PrivacyPolicySectionAdmin(TranslationAdmin):
+	list_display = ('title', 'position', 'last_update_date')
+
+	fields = ('title', 'text', 'position')
+	formfield_overrides = {models.TextField: {'widget': TinyMCE}}
