08216a97d5dcca290e82afd70458dbb365493636
EXG1O <borodenko22448811@gmail.com>
2024-01-23 14:13:45 +0200

    Added tests for updates app

--- updates/urls.py
+++ updates/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import UpdatesAPIView
 
 
+app_name = 'updates'
 urlpatterns = [
-	path('', UpdatesAPIView.as_view()),
+	path('', UpdatesAPIView.as_view(), name='index'),
 ]
