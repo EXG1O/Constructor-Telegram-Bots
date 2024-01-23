514b8ea12a1202e7df88fdd72f52f9b518cca4b5
EXG1O <borodenko22448811@gmail.com>
2024-01-23 13:41:31 +0200

    Upgraded CookiesTokenAuthentication

--- constructor_telegram_bots/authentication.py
+++ constructor_telegram_bots/authentication.py
@@ -1,23 +1,32 @@
 from django.utils.translation import gettext as _
 
-from rest_framework.authentication import BaseAuthentication
-from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.request import Request
 from rest_framework.authtoken.models import Token
 
 from user.models import User
 
 
-class CookiesTokenAuthentication(BaseAuthentication):
-	def authenticate(self, request: Request) -> tuple[User, str] | None:
-		auth_token: str | None  = request.COOKIES.get('auth-token')
+class CookiesTokenAuthentication(TokenAuthentication):
+	"""
+	Simple token based authentication that supports authentication via cookies.
 
-		if not auth_token:
-			return None
+	Clients can authenticate by passing the token key either in the `Authorization`
+	HTTP heade or by storing the token in a cookie named `auth-token`.
+
+	Authorization header example:
+		Authorization: Token 401f7ac837da42b97f613d789819ff93537bee6a
+
+	Cookie example:
+		auth-token=401f7ac837da42b97f613d789819ff93537bee6a
+	"""
+
+	def authenticate(self, request: Request) -> tuple[User, str] | None:
+		auth_token: str | None = request.COOKIES.get('auth-token')
 
 		try:
 			token: Token = Token.objects.get(key=auth_token)
-		except Token.DoesNotExist:
-			raise AuthenticationFailed(_('Неверный токен.'))
 
-		return (token.user, auth_token)
+			return self.authenticate_credentials(token.key)
+		except Token.DoesNotExist:
+			return super().authenticate(request)
