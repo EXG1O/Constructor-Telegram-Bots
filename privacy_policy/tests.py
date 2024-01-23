c3ad45c20dde9ee339232c788a7146bb27e3e00d
EXG1O <borodenko22448811@gmail.com>
2024-01-23 14:18:30 +0200

    Added tests for privacy_policy app

--- privacy_policy/tests.py
+++ privacy_policy/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.http import HttpResponse
+from django.urls import reverse
 
-# Create your tests here.
+from rest_framework.test import APIClient
+
+
+class PrivacyPolicySectionsAPIViewTests(TestCase):
+	url: str = reverse('api:privacy-policy:sections')
+
+	def setUp(self) -> None:
+		self.client: APIClient = APIClient()
+
+	def test_get_method(self) -> None:
+		response: HttpResponse = self.client.get(self.url)
+		self.assertEqual(response.status_code, 200)
--- privacy_policy/urls.py
+++ privacy_policy/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import PrivacyPolicySectionsAPIView
 
 
+app_name = 'privacy-policy'
 urlpatterns = [
-	path('sections/', PrivacyPolicySectionsAPIView.as_view()),
+	path('sections/', PrivacyPolicySectionsAPIView.as_view(), name='sections'),
 ]
