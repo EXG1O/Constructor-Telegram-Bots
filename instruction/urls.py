0edc192f603fc8968d88b655040aca014af34a49
EXG1O <borodenko22448811@gmail.com>
2024-01-23 14:19:39 +0200

    Added tests for instruction app

--- instruction/urls.py
+++ instruction/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import InstructionSectionsAPIView
 
 
+app_name = 'instruction'
 urlpatterns = [
-	path('sections/', InstructionSectionsAPIView.as_view()),
+	path('sections/', InstructionSectionsAPIView.as_view(), name='sections'),
 ]
