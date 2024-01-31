993029b8f3980ff54472e0c37b7b16c9cd555fb1
EXG1O <borodenko22448811@gmail.com>
2024-01-31 21:08:58 +0200

    Refactored Files component of Constructor router

--- frontend/src/routes/AuthRequired/TelegramBotMenu/Constructor/components/CommandOffcanvas/components/Files.tsx
+++ frontend/src/routes/AuthRequired/TelegramBotMenu/Constructor/components/CommandOffcanvas/components/Files.tsx
@@ -6,11 +6,10 @@ import Button from 'react-bootstrap/Button';
 
 import useToast from 'services/hooks/useToast';
 
-interface FileData {
+interface FileData extends Pick<File, 'name' | 'size'> {
 	id?: number;
+	key: string;
 	file?: File;
-	name: File['name'];
-	size: File['size'];
 }
 
 export type Data = FileData[];
@@ -30,62 +29,56 @@ function Files({ initialData, onChange, ...props }: FilesProps): ReactElement<Fi
 	function handleFilesChange(event: ReactChangeEvent<HTMLInputElement>): void {
 		if (event.target.files) {
 			const files: File[] = [...event.target.files];
-			const filesResult: File[] = []
 
 			event.target.value = '';
 
-			for (const file of files) {
-				if (file.size < 3145728) {
-					let isFinded = false;
-
-					for (const file_ of data) {
-						if (
-							file_.name === file.name &&
-							file_.size === file.size
-						) {
-							isFinded = true;
-							break;
+			setData([
+				...data,
+				...files.filter(file => {
+					if (file.size < 3145728) {
+						for (const _file of data) {
+							if (
+								_file.name === file.name &&
+								_file.size === file.size
+							) {
+								createMessageToast({
+									message: interpolate(
+										gettext('Файл %(name)s уже добавлен!'),
+										{ name: file.name },
+										true,
+									),
+									level: 'error',
+								});
+								return false;
+							}
 						}
-					}
 
-					if (!isFinded) {
-						filesResult.push(file);
+						return true;
 					} else {
 						createMessageToast({
 							message: interpolate(
-								gettext('Файл %(name)s уже добавлен!'),
+								gettext('Файл %(name)s весит больше 3МБ!'),
 								{ name: file.name },
 								true,
 							),
 							level: 'error',
 						});
 					}
-				} else {
-					createMessageToast({
-						message: interpolate(
-							gettext('Файл %(name)s весит больше 3МБ!'),
-							{ name: file.name },
-							true,
-						),
-						level: 'error',
-					});
-				}
-			}
-
-			setData([
-				...data,
-				...Object.keys(filesResult).map(key => {
-					const file: File = filesResult[parseInt(key)];
-
-					return { file, name: file.name, size: file.size };
-				}),
+				}).map(file => ({
+					key: crypto.randomUUID(),
+					file,
+					name: file.name,
+					size: file.size,
+				})),
 			]);
 		}
 	}
 
 	function handleDeleteFileButtonClick(index: number): void {
 		const files = [...data];
+
 		files.splice(index, 1);
+
 		setData(files);
 	}
 
@@ -95,10 +88,10 @@ function Files({ initialData, onChange, ...props }: FilesProps): ReactElement<Fi
 				{gettext('Файлы')}
 			</Card.Header>
 			<Card.Body className='vstack gap-2 p-2'>
-				{data.length ? (
-					<div className='d-flex flex-wrap border rounded gap-1 p-1'>
+				{Boolean(data.length) && (
+					<div className='d-flex flex-wrap border rounded-1 gap-1 p-1'>
 						{data.map((file, index) => (
-							<ButtonGroup key={index}>
+							<ButtonGroup key={file.key}>
 								<small className='text-bg-dark rounded-1 rounded-end-0 px-2 py-1'>
 									{file.name}
 								</small>
@@ -113,7 +106,7 @@ function Files({ initialData, onChange, ...props }: FilesProps): ReactElement<Fi
 							</ButtonGroup>
 						))}
 					</div>
-				) : undefined}
+				)}
 				<input
 					id='command-offcanvas-files-input-file'
 					type='file'
