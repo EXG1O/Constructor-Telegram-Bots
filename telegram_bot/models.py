f9710628b5c3e7d223f3f98b46d916a2cd8511b7
EXG1O <borodenko22448811@gmail.com>
2024-02-20 17:41:32 +0200

    Updated full_name field of TelegramBotUser model

--- telegram_bot/models.py
+++ telegram_bot/models.py
@@ -217,7 +217,7 @@ class TelegramBotVariable(models.Model):
 class TelegramBotUser(models.Model):
 	telegram_bot = models.ForeignKey(TelegramBot, on_delete=models.CASCADE, related_name='users', verbose_name=_('Telegram бот'))
 	telegram_id = models.BigIntegerField('Telegram ID')
-	full_name = models.CharField(_('Полное имя'), max_length=129, null=True)
+	full_name = models.CharField(_('Имя и фамилия'), max_length=129)
 	is_allowed = models.BooleanField(_('Разрешён'), default=False)
 	is_blocked = models.BooleanField(_('Заблокирован'), default=False)
 	last_activity_date = models.DateTimeField(_('Дата последней активности'), auto_now_add=True, null=True)
@@ -230,4 +230,4 @@ class TelegramBotUser(models.Model):
 		verbose_name_plural = _('Пользователи')
 
 	def __str__(self) -> str:
-		return self.full_name if self.full_name else str(self.telegram_id)
+		return self.full_name
