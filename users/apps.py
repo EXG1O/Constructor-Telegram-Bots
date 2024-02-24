1c6f392d07a8397027b2f01b58cb63c0bcd9ac26
EXG1O <borodenko22448811@gmail.com>
2024-02-24 13:43:38 +0200

    Renamed apps classes of users app

--- users/apps.py
+++ users/apps.py
@@ -2,7 +2,7 @@ from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
 
-class UserConfig(AppConfig):
+class UsersConfig(AppConfig):
 	default_auto_field = 'django.db.models.BigAutoField'
 	name = 'users'
 	verbose_name = _('Пользователи')
