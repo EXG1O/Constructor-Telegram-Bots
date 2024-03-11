63187ceb6dbd35a6e53780414becf4882a7d3a5c
EXG1O <borodenko22448811@gmail.com>
2024-03-11 18:54:36 +0200

    Add stats for Home route of frontend

--- frontend/src/routes/Home/components/Stats/index.tsx
+++ frontend/src/routes/Home/components/Stats/index.tsx
@@ -1,4 +1,5 @@
 import React, { ReactElement } from 'react';
+import { useRouteLoaderData } from 'react-router-dom';
 
 import './index.scss';
 
@@ -6,14 +7,27 @@ import Stack from 'react-bootstrap/Stack';
 
 import InfoArea from './components/InfoArea';
 
+import { LoaderData as HomeLoaderData } from '../..';
+
 function Stats(): ReactElement {
+	const { stats } = useRouteLoaderData('home') as HomeLoaderData;
+
 	return (
 		<div className='stats'>
 			<h3>{gettext('Информация о сайте')}</h3>
 			<Stack gap={1}>
-				<InfoArea value={0}>{gettext('Количество пользователей')}</InfoArea>
-				<InfoArea value={1}>{gettext('Количество добавленных Telegram ботов')}</InfoArea>
-				<InfoArea value={1}>{gettext('Количество включенных Telegram ботов')}</InfoArea>
+				<InfoArea value={stats.users.count}>
+					{gettext('Пользователей')}
+				</InfoArea>
+				<InfoArea value={stats.telegramBots.telegram_bots.total}>
+					{gettext('Добавленных Telegram ботов')}
+				</InfoArea>
+				<InfoArea value={stats.telegramBots.telegram_bots.enabled}>
+					{gettext('Включенных Telegram ботов')}
+				</InfoArea>
+				<InfoArea value={stats.telegramBots.users.total}>
+					{gettext('Пользователей Telegram ботов')}
+				</InfoArea>
 			</Stack>
 		</div>
 	);
--- frontend/src/routes/Home/index.tsx
+++ frontend/src/routes/Home/index.tsx
@@ -1,5 +1,4 @@
 import React, { ReactElement } from 'react';
-import { json } from 'react-router-dom';
 
 import Container from 'react-bootstrap/Container';
 
@@ -9,21 +8,51 @@ import Header from './components/Header';
 import Stats from './components/Stats';
 import Donations from './components/Donations';
 
+import { StatsAPI as UsersStatsAPI } from 'services/api/users/main';
+import { APIResponse as UsersAPIResponse } from 'services/api/users/types';
+
+import { StatsAPI as TelegramBotsStatsAPI } from 'services/api/telegram_bots/main';
+import { APIResponse as TelegramBotsAPIResponse } from 'services/api/telegram_bots/types';
+
 import { DonationsAPI } from 'services/api/donations/main';
-import { APIResponse } from 'services/api/donations/types';
+import { APIResponse as DonationsAPIResponse } from 'services/api/donations/types';
+
+interface LoaderDataStats {
+	users: UsersAPIResponse.StatsAPI.Get;
+	telegramBots: TelegramBotsAPIResponse.StatsAPI.Get;
+}
 
 export interface LoaderData {
-	donations: APIResponse.DonationsAPI.Get.Pagination;
+	stats: LoaderDataStats;
+	donations: DonationsAPIResponse.DonationsAPI.Get.Pagination;
 }
 
 export async function loader(): Promise<LoaderData> {
-	const response = await DonationsAPI.get(20);
+	const [
+		usersStatsResponse,
+		telegramBotsResponse,
+		donationsResponse,
+	] = await Promise.all([
+		UsersStatsAPI.get(),
+		TelegramBotsStatsAPI.get(),
+		DonationsAPI.get(20),
+	]);
 
-	if (!response.ok) {
-		throw json(response.json, response.status);
+	if (
+		!usersStatsResponse.ok ||
+		!telegramBotsResponse.ok ||
+		!donationsResponse.ok
+	) {
+		throw Error('Failed to fetch data!');
 	}
 
-	return { donations: response.json };
+	return {
+		stats: {
+			users: usersStatsResponse.json,
+			telegramBots: telegramBotsResponse.json,
+		},
+		donations: donationsResponse.json,
+	}
 }
 
 function Home(): ReactElement {
