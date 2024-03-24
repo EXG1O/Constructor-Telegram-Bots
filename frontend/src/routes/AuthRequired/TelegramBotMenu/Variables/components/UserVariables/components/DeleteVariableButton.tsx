f55985460f407dfe47c6810e907332ce8c04b264
EXG1O <borodenko22448811@gmail.com>
2024-03-24 03:45:34 +0200

    Create DeleteVariableButton component for VariableDisplay component

---
+++ frontend/src/routes/AuthRequired/TelegramBotMenu/Variables/components/UserVariables/components/DeleteVariableButton.tsx
@@ -0,0 +1,65 @@
+import React, { ReactElement, HTMLAttributes, memo, useCallback, useState } from 'react';
+import { useRouteLoaderData } from 'react-router-dom';
+import classNames from 'classnames';
+
+import AskConfirmModal from 'components/AskConfirmModal';
+
+import useToast from 'services/hooks/useToast';
+import useVariables from '../hooks/useVariables';
+
+import { LoaderData as TelegramBotMenuRootLoaderData } from 'routes/AuthRequired/TelegramBotMenu/Root';
+
+import { VariableAPI } from 'services/api/telegram_bots/main';
+import { Variable } from 'services/api/telegram_bots/types';
+
+export interface DeleteVariableButtonProps extends Pick<HTMLAttributes<HTMLElement>, 'className'> {
+	variable: Variable;
+}
+
+function DeleteVariableButton({ variable, className }: DeleteVariableButtonProps): ReactElement<DeleteVariableButtonProps> {
+	const { telegramBot } = useRouteLoaderData('telegram-bot-menu-root') as TelegramBotMenuRootLoaderData;
+
+	const { createMessageToast } = useToast();
+	const { updateVariables } = useVariables();
+
+	const [showModal, setShowModal] = useState<boolean>(false);
+	const [loadingModal, setLoadingModal] = useState<boolean>(false);
+
+	const handleConfirmDelete = useCallback(async () => {
+		setLoadingModal(true);
+
+		const response = await VariableAPI._delete(telegramBot.id, variable.id);
+
+		if (response.ok) {
+			updateVariables();
+			setShowModal(false);
+		}
+
+		setLoadingModal(false);
+		createMessageToast({
+			message: response.json.message,
+			level: response.json.level,
+		});
+	}, []);
+
+	return (
+		<>
+			<AskConfirmModal
+				show={showModal}
+				loading={loadingModal}
+				title={gettext('Удаление переменной')}
+				onConfirm={handleConfirmDelete}
+				onHide={useCallback(() => setShowModal(false), [])}
+			>
+				{gettext('Вы точно хотите удалить переменную?')}
+			</AskConfirmModal>
+			<i
+				className={classNames('d-flex text-danger bi bi-trash', className)}
+				style={{ fontSize: '18px', cursor: 'pointer' }}
+				onClick={() => setShowModal(true)}
+			/>
+		</>
+	);
+}
+
+export default memo(DeleteVariableButton);
--- frontend/src/routes/AuthRequired/TelegramBotMenu/Variables/components/UserVariables/components/VariableDisplay.tsx
+++ frontend/src/routes/AuthRequired/TelegramBotMenu/Variables/components/UserVariables/components/VariableDisplay.tsx
@@ -1,16 +1,8 @@
-import React, { ReactElement, memo, useCallback, useState } from 'react';
-import { useRouteLoaderData } from 'react-router-dom';
-
-import AskConfirmModal from 'components/AskConfirmModal';
+import React, { ReactElement, memo } from 'react';
 
 import EditVariableButton from './EditVariableButton';
+import DeleteVariableButton from './DeleteVariableButton';
 
-import useToast from 'services/hooks/useToast';
-import useVariables from '../hooks/useVariables';
-
-import { LoaderData as TelegramBotMenuRootLoaderData } from 'routes/AuthRequired/TelegramBotMenu/Root';
-
-import { VariableAPI } from 'services/api/telegram_bots/main';
 import { Variable } from 'services/api/telegram_bots/types';
 
 export interface VariableDisplayProps {
@@ -18,72 +10,32 @@ export interface VariableDisplayProps {
 }
 
 function VariableDisplay({ variable }: VariableDisplayProps): ReactElement<VariableDisplayProps> {
-	const { telegramBot } = useRouteLoaderData('telegram-bot-menu-root') as TelegramBotMenuRootLoaderData;
-
-	const { createMessageToast } = useToast();
-	const { updateVariables } = useVariables();
-
-	const [showDeletionModal, setShowDeletionModal] = useState<boolean>(false);
-	const [loadingDeletionModal, setLoadingDeletionModal] = useState<boolean>(false);
-
-	const handleConfirmDelete = useCallback(async () => {
-		setLoadingDeletionModal(true);
-
-		const response = await VariableAPI._delete(telegramBot.id, variable.id);
-
-		if (response.ok) {
-			updateVariables();
-			setShowDeletionModal(false);
-		}
-
-		setLoadingDeletionModal(false);
-		createMessageToast({
-			message: response.json.message,
-			level: response.json.level,
-		});
-	}, []);
-
 	return(
-		<>
-			<AskConfirmModal
-				show={showDeletionModal}
-				loading={loadingDeletionModal}
-				title={gettext('Удаление переменной')}
-				onConfirm={handleConfirmDelete}
-				onHide={useCallback(() => setShowDeletionModal(false), [])}
-			>
-				{gettext('Вы точно хотите удалить переменную?')}
-			</AskConfirmModal>
-			<tr>
-				<td className='w-50'>
-					<div className='d-flex gap-2'>
-						<i
-							className='btn-clipboard bi bi-clipboard'
-							data-clipboard-text={`{{ ${variable.name} }}`}
-							style={{ cursor: 'pointer' }}
-						/>
-						<span className='flex-fill text-info-emphasis'>
-							{variable.name}
-						</span>
+		<tr>
+			<td className='w-50'>
+				<div className='d-flex gap-2'>
+					<i
+						className='btn-clipboard bi bi-clipboard'
+						data-clipboard-text={`{{ ${variable.name} }}`}
+						style={{ cursor: 'pointer' }}
+					/>
+					<span className='flex-fill text-info-emphasis'>
+						{variable.name}
+					</span>
+				</div>
+			</td>
+			<td>
+				<div className='d-flex gap-2'>
+					<span className='flex-fill text-nowrap'>
+						{variable.description}
+					</span>
+					<div className='d-flex gap-1'>
+						<EditVariableButton variable={variable} className='my-auto' />
+						<DeleteVariableButton variable={variable} className='my-auto' />
 					</div>
-				</td>
-				<td>
-					<div className='d-flex gap-2'>
-						<span className='flex-fill text-nowrap'>
-							{variable.description}
-						</span>
-						<div className='d-flex gap-1'>
-							<EditVariableButton variable={variable} className='my-auto' />
-							<i
-								className='d-flex text-danger bi bi-trash my-auto'
-								style={{ fontSize: '18px', cursor: 'pointer' }}
-								onClick={() => setShowDeletionModal(true)}
-							/>
-						</div>
-					</div>
-				</td>
-			</tr>
-		</>
+				</div>
+			</td>
+		</tr>
 	);
 }
 
