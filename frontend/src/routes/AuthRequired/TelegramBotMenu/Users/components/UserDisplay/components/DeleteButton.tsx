a060a94d53c0ca73dd4e30db9400195770699cd6
EXG1O <borodenko22448811@gmail.com>
2024-04-02 05:19:54 +0300

    Create DeleteButton component for UserDisplay component of Users route

--- frontend/src/routes/AuthRequired/TelegramBotMenu/Users/components/UserDisplay/components/DeleteButton.tsx
+++ frontend/src/routes/AuthRequired/TelegramBotMenu/Users/components/UserDisplay/components/DeleteButton.tsx
@@ -1,37 +1,43 @@
-import React, { ReactNode, memo, useCallback, useState } from 'react';
+import React, { ReactElement, HTMLAttributes, MouseEvent, memo, useCallback, useState } from 'react'
 import { useRouteLoaderData } from 'react-router-dom';
+import classNames from 'classnames';
 
 import AskConfirmModal from 'components/AskConfirmModal';
 
 import useToast from 'services/hooks/useToast';
-import useUsers from '../hooks/useUsers';
+import useUsers from '../../../hooks/useUsers';
 
 import { LoaderData as TelegramBotMenuRootLoaderData } from 'routes/AuthRequired/TelegramBotMenu/Root';
 
 import { UserAPI } from 'services/api/telegram_bots/main';
 import { User } from 'services/api/telegram_bots/types';
 
-export interface UserDisplayProps {
+export interface DeleteButtonProps extends HTMLAttributes<HTMLElement> {
 	user: User;
 }
 
-function UserDisplay({ user }: UserDisplayProps): ReactNode {
+function DeleteButton({ user, className, style, onClick, ...props }: DeleteButtonProps): ReactElement<DeleteButtonProps> {
 	const { telegramBot } = useRouteLoaderData('telegram-bot-menu-root') as TelegramBotMenuRootLoaderData;
 
 	const { createMessageToast } = useToast();
 	const { updateUsers } = useUsers();
 
-	const [showDeleteUserModal, setShowDeleteUserModal] = useState<boolean>(false);
-	const [loadingDeleteUserModal, setLoadingDeleteUserModal] = useState<boolean>(false);
+	const [showModal, setShowModal] = useState<boolean>(false);
+	const [loadingModal, setLoadingModal] = useState<boolean>(false);
 
-	async function handleConfirmDelete(): Promise<void> {
-		setLoadingDeleteUserModal(true);
+	function handleClick(event: MouseEvent<HTMLElement>): void {
+		setShowModal(true);
+		onClick?.(event);
+	}
+
+	const handleConfirm = useCallback(async () => {
+		setLoadingModal(true);
 
 		const response = await UserAPI._delete(telegramBot.id, user.id);
 
 		if (response.ok) {
 			updateUsers();
-			setShowDeleteUserModal(false);
+			setShowModal(false);
 			createMessageToast({
 				message: gettext('Вы успешно удалили пользователя.'),
 				level: 'success',
@@ -43,34 +49,28 @@ function UserDisplay({ user }: UserDisplayProps): ReactNode {
 			});
 		}
 
-		setLoadingDeleteUserModal(false);
-	}
+		setLoadingModal(false);
+	}, []);
 
 	return (
 		<>
 			<AskConfirmModal
-				show={showDeleteUserModal}
-				loading={loadingDeleteUserModal}
+				show={showModal}
+				loading={loadingModal}
 				title={gettext('Удаление пользователя')}
-				onConfirm={handleConfirmDelete}
-				onHide={useCallback(() => setShowDeleteUserModal(false), [])}
+				onConfirm={handleConfirm}
+				onHide={useCallback(() => setShowModal(false), [])}
 			>
 				{gettext('Вы точно хотите удалить пользователя?')}
 			</AskConfirmModal>
-			<tr>
-				<td className='text-success-emphasis'>{`[${user.activated_date}]`}</td>
-				<td className='text-primary-emphasis'>{user.telegram_id}</td>
-				<td className='w-100'>{user.full_name}</td>
-				<td>
-					<i
-						className='d-flex bi bi-trash text-danger'
-						style={{ fontSize: '18px', cursor: 'pointer' }}
-						onClick={() => setShowDeleteUserModal(true)}
-					/>
-				</td>
-			</tr>
+			<i
+				{...props}
+				className={classNames('d-flex bi bi-trash text-danger', className)}
+				style={{ fontSize: '18px', cursor: 'pointer', ...style }}
+				onClick={handleClick}
+			/>
 		</>
 	);
 }
 
-export default memo(UserDisplay);
+export default memo(DeleteButton);
---
+++ frontend/src/routes/AuthRequired/TelegramBotMenu/Users/components/UserDisplay/index.tsx
@@ -0,0 +1,23 @@
+import React, { ReactNode, memo } from 'react';
+
+import { User } from 'services/api/telegram_bots/types';
+import DeleteButton from './components/DeleteButton';
+
+export interface UserDisplayProps {
+	user: User;
+}
+
+function UserDisplay({ user }: UserDisplayProps): ReactNode {
+	return (
+		<tr>
+			<td className='text-success-emphasis'>{`[${user.activated_date}]`}</td>
+			<td className='text-primary-emphasis'>{user.telegram_id}</td>
+			<td className='w-100'>{user.full_name}</td>
+			<td>
+				<DeleteButton user={user} />
+			</td>
+		</tr>
+	);
+}
+
+export default memo(UserDisplay);
