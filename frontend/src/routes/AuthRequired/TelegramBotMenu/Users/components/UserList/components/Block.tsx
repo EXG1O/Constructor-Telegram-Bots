7fa7a41144edcf1bb0a735fa58466101bc10dfe4
EXG1O <borodenko22448811@gmail.com>
2024-04-23 04:45:54 +0300

    Update className prop for UserList.Block component of frontend Users route

--- frontend/src/routes/AuthRequired/TelegramBotMenu/Users/components/UserList/components/Block.tsx
+++ frontend/src/routes/AuthRequired/TelegramBotMenu/Users/components/UserList/components/Block.tsx
@@ -4,7 +4,7 @@ import classNames from 'classnames';
 export type BlockProps = HTMLAttributes<HTMLDivElement>;
 
 function Block({ className, ...props }: BlockProps): ReactElement {
-	return <div {...props} className={classNames('border rounded', className)} />;
+	return <div {...props} className={classNames('border rounded-1', className)} />;
 }
 
 export default Block
