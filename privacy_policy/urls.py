8ea6ec22807ef0cf3103758d02065c6f425ab994
EXG1O <borodenko22448811@gmail.com>
2024-04-30 14:55:36 +0300

    Update urls for privacy_policy and updates apps

--- privacy_policy/urls.py
+++ privacy_policy/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import SimpleRouter
 from .views import SectionsViewSet
 
 router = SimpleRouter()
-router.register('sections', SectionsViewSet)
+router.register('sections', SectionsViewSet, basename='section')
 
 app_name = 'privacy-policy'
 urlpatterns = router.urls
