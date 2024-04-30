ca1f76cc45f787148eda71b2423f61409bb5e7e7
EXG1O <borodenko22448811@gmail.com>
2024-04-30 15:09:18 +0300

    Update urls for privacy_policy, updates, instruction apps

--- updates/urls.py
+++ updates/urls.py
@@ -2,7 +2,7 @@ from rest_framework.routers import SimpleRouter
 
 from .views import UpdateViewSet
 
-router = SimpleRouter()
+router = SimpleRouter(use_regex_path=False)  # type: ignore [call-arg]  # use_regex_path param exists
 router.register('updates', UpdateViewSet, basename='update')
 
 app_name = 'updates'
