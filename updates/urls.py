9cd48bc633c83796bb99722557f92ac8a2fdae31
EXG1O <borodenko22448811@gmail.com>
2024-05-04 16:31:04 +0300

    Update urls for updates and donation apps

--- updates/urls.py
+++ updates/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import SimpleRouter
 from .views import UpdateViewSet
 
 router = SimpleRouter(use_regex_path=False)  # type: ignore [call-arg]  # use_regex_path param exists
-router.register('updates', UpdateViewSet, basename='update')
+router.register('', UpdateViewSet, basename='update')
 
 app_name = 'updates'
 urlpatterns = router.urls
