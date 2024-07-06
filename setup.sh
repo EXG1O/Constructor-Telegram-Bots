1514890e689f6ce197ca0d0247c074c3fbd65c41
EXG1O <borodenko22448811@gmail.com>
2024-07-07 01:34:31 +0300

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-python -m venv env
-source env/bin/activate
-
 pip install -U pip
-pip install -r requirements.txt
+pip install poetry
+poetry install
 
-SECRET_KEY=$(python -c "import random, string; print(f\"django-insecure-{''.join([random.choice(string.ascii_letters + string.digits) for num in range(50)])}\")")
+SECRET_KEY=$(python -c "import random, string; print(''.join(random.choices(string.ascii_letters + string.digits, k=50)))")
 
-read -p "Enter Telegram bot username: " TELEGRAM_BOT_USERNAME
-read -p "Enter Telegram bot API-Token: " TELEGRAM_BOT_TOKEN
 read -p "Enter PostgreSQL database name: " POSTGRESQL_DATABASE_NAME
 read -p "Enter PostgreSQL database user: " POSTGRESQL_DATABASE_USER
 read -p "Enter PostgreSQL database password: " POSTGRESQL_DATABASE_PASSWORD
@@ -18,8 +14,8 @@ cat << EOF > .env
 SECRET_KEY='$SECRET_KEY'
 DEBUG=True
 
-TELEGRAM_BOT_USERNAME=$TELEGRAM_BOT_USERNAME
-TELEGRAM_BOT_TOKEN=$TELEGRAM_BOT_TOKEN
+FRONTEND_PATH=$PWD/frontend/dist
+TELEGRAM_BOTS_HUB_PATH=$PWD/services/telegram_bots_hub
 
 POSTGRESQL_DATABASE_NAME=$POSTGRESQL_DATABASE_NAME
 POSTGRESQL_DATABASE_USER=$POSTGRESQL_DATABASE_USER
