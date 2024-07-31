3a880f807d2f0aa4c8d0232d7ca77c8ac09f6da5
exg1o <borodenko22448811@gmail.com>
2024-07-31 23:05:46 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,18 +4,17 @@ pip install -U pip
 pip install poetry
 poetry install
 
-SECRET_KEY=$(python -c "import random, string; print(''.join(random.choices(string.ascii_letters + string.digits, k=50)))")
+SECRET_KEY=$(python -c "import secrets; print(secrets.token_hex(25))")
 
 read -p "Enter PostgreSQL database name: " POSTGRESQL_DATABASE_NAME
 read -p "Enter PostgreSQL database user: " POSTGRESQL_DATABASE_USER
 read -p "Enter PostgreSQL database password: " POSTGRESQL_DATABASE_PASSWORD
 
 cat << EOF > .env
-SECRET_KEY='$SECRET_KEY'
+SECRET_KEY=$SECRET_KEY
 DEBUG=True
 
 FRONTEND_PATH=$PWD/frontend/dist
-TELEGRAM_BOTS_HUB_PATH=$PWD/services/telegram_bots_hub
 
 POSTGRESQL_DATABASE_NAME=$POSTGRESQL_DATABASE_NAME
 POSTGRESQL_DATABASE_USER=$POSTGRESQL_DATABASE_USER
