7ae1eeb106e287b1a40cec22beaa8f2e03a3057b
exg1o <borodenko22448811@gmail.com>
2024-09-08 20:11:51 +0300

    Update `TelegramBotMixin` class

--- telegram_bots/mixins.py
+++ telegram_bots/mixins.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from django.utils.functional import cached_property
 
 from .models import TelegramBot
 
@@ -6,17 +7,12 @@ from typing import Any
 
 
 class TelegramBotMixin:
-	_telegram_bot: TelegramBot | None = None
-
-	@property
+	@cached_property
 	def telegram_bot(self) -> TelegramBot:
-		if self._telegram_bot is None:
-			self._telegram_bot = get_object_or_404(
-				self.request.user.telegram_bots,  # type: ignore [attr-defined]
-				id=self.kwargs['telegram_bot_id'],  # type: ignore [attr-defined]
-			)
-
-		return self._telegram_bot
+		return get_object_or_404(
+			self.request.user.telegram_bots,  # type: ignore [attr-defined]
+			id=self.kwargs['telegram_bot_id'],  # type: ignore [attr-defined]
+		)
 
 	def get_serializer_context(self) -> dict[str, Any]:
 		context: dict[str, Any] = super().get_serializer_context()  # type: ignore [misc]
