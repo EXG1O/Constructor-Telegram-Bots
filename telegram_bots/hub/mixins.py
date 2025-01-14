82f3f76c9dac9befdce58b0ded3e0bd200704b0a
exg1o <borodenko22448811@gmail.com>
2025-01-14 22:03:38 +0200

    Update mixins of `telegram_bots/hub` app

--- telegram_bots/hub/mixins.py
+++ telegram_bots/hub/mixins.py
@@ -3,16 +3,16 @@ from django.utils.functional import cached_property
 
 from ..models import TelegramBot
 
-from typing import Any
+from typing import TYPE_CHECKING, Any
 
 
 class TelegramBotMixin:
+	if TYPE_CHECKING:
+		kwargs: dict[str, Any]
+
 	@cached_property
 	def telegram_bot(self) -> TelegramBot:
-		return get_object_or_404(
-			self.request.user.telegram_bots,  # type: ignore [attr-defined]
-			id=self.kwargs['telegram_bot_id'],  # type: ignore [attr-defined]
-		)
+		return get_object_or_404(TelegramBot, id=self.kwargs['telegram_bot_id'])
 
 	def get_serializer_context(self) -> dict[str, Any]:
 		context: dict[str, Any] = super().get_serializer_context()  # type: ignore [misc]
