5afee33ac180476019179747facbcd20ec977744
exg1o <77574092+EXG1O@users.noreply.github.com>
2025-01-20 16:47:14 +0200

    Improve `MultiPartJSONParser` parser (#125)
    Now it's no longer tied only to the `image` or `file` prefixes, making it more independent and helping us in the future with new functionality.

--- constructor_telegram_bots/parsers.py
+++ constructor_telegram_bots/parsers.py
@@ -1,9 +1,11 @@
 from django.core.files.uploadedfile import UploadedFile
+from django.utils.datastructures import MultiValueDict
 from django.utils.translation import gettext_lazy as _
 
 from rest_framework.exceptions import ParseError
 from rest_framework.parsers import DataAndFiles, MultiPartParser
 
+from collections import defaultdict
 from itertools import chain
 from json import JSONDecodeError
 from typing import Any
@@ -16,35 +18,53 @@ class MultiPartJSONParser(MultiPartParser):
 
 	error_detail = _("Не удалось проанализировать JSON данные для ключа '%(key)s'.")
 
-	def parse_json(self, json_string: str, key: str) -> dict[str, Any]:
+	def parse_json(self, key: str, data: str) -> dict[str, Any]:
 		try:
-			return json.loads(json_string)
+			return json.loads(data)
 		except JSONDecodeError as error:
 			raise ParseError(self.error_detail % {'key': key}) from error
 
 	def parse(self, *args: Any, **kwargs: Any) -> dict[str, Any]:  # type: ignore [override]
-		parsed: DataAndFiles = super().parse(*args, **kwargs)  # type: ignore [type-arg]
+		parsed: DataAndFiles[dict[str, Any], MultiValueDict[str, UploadedFile]] = (
+			super().parse(*args, **kwargs)  # type: ignore [assignment]
+		)
 
-		data: dict[str, Any] = self.parse_json(parsed.data.get('data', '{}'), 'data')
-		data.update({'images': [], 'files': []})
+		data: dict[str, Any] = (
+			self.parse_json('data', raw_data)
+			if (raw_data := parsed.data.get('data'))
+			else {}
+		)
+		tmp_data: defaultdict[str, list[dict[str, Any]]] = defaultdict(list)
 
 		for key, value in chain(parsed.data.items(), parsed.files.items()):
-			if not re.fullmatch(r'^(image|file):\d+$', key, re.IGNORECASE):
+			result: re.Match[str] | None = re.fullmatch(
+				r'^(?P<name>\w+):(?P<index>\d+)$', key, re.IGNORECASE
+			)
+
+			if not result:
 				continue
 
-			name, index = key.split(':')
+			name, index = result.group('name', 'index')
 
 			extra_data_key: str = f'{name}:{index}:extra_data'
-			extra_data: dict[str, Any] = self.parse_json(
-				parsed.data.get(extra_data_key, '{}'), extra_data_key
+			extra_data: dict[str, Any] = (
+				self.parse_json(extra_data_key, raw_extra_data)
+				if (raw_extra_data := parsed.data.get(extra_data_key))
+				else {}
 			)
 
 			if isinstance(value, UploadedFile):
-				data[f'{name}s'].append({name: value, **extra_data})
+				tmp_data[name].append({name: value, **extra_data})
 			elif isinstance(value, str):
 				if value.isdigit():
-					data[f'{name}s'].append({'id': int(value), **extra_data})
+					tmp_data[name].append({'id': int(value), **extra_data})
 				else:
-					data[f'{name}s'].append({'url': value, **extra_data})
+					tmp_data[name].append({'url': value, **extra_data})
+
+		for key, items in tmp_data.items():
+			if len(items) > 1:
+				data.update({f'{key}s': items})
+			else:
+				data.update({key: items[0]})
 
 		return data
