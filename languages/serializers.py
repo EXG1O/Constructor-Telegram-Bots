fd2283d0b780f91e4f6903fdb49a1500887e3034
exg1o <77574092+EXG1O@users.noreply.github.com>
2025-07-18 14:14:23 +0300

    Refactor `languages` app (#170)

--- languages/serializers.py
+++ languages/serializers.py
@@ -2,10 +2,8 @@ from django.conf import settings
 
 from rest_framework import serializers
 
-from typing import Any
 
-
-class SetLanguageSerializer(serializers.Serializer[Any]):
+class SetLanguageSerializer(serializers.Serializer[None]):
     lang_code = serializers.ChoiceField(settings.LANGUAGES)
 
     class Meta:
