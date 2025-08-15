5c04c447da650d2126283927783407707cb9b908
exg1o <exg1o.me@gmail.com>
2025-08-15 03:45:14 +0300

    Fix `views` of `telegram_bots/hub` app

--- telegram_bots/hub/views/__init__.py
+++ telegram_bots/hub/views/__init__.py
@@ -2,7 +2,6 @@ from .api_request import APIRequestViewSet
 from .background_task import BackgroundTaskViewSet
 from .command import CommandKeyboardButtonViewSet, CommandViewSet
 from .condition import ConditionViewSet
-from .connection import ConnectionViewSet
 from .database_operation import DatabaseOperationViewSet
 from .database_record import DatabaseRecordViewSet
 from .telegram_bot import TelegramBotViewSet
@@ -12,7 +11,6 @@ from .variable import VariableViewSet
 
 __all__ = [
     'TelegramBotViewSet',
-    'ConnectionViewSet',
     'TriggerViewSet',
     'CommandViewSet',
     'CommandKeyboardButtonViewSet',
