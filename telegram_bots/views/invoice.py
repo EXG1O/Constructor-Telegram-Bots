55009433d49fa76c664d5fa79d1211dd0f07b4e3
exg1o <77574092+EXG1O@users.noreply.github.com>
2026-01-05 00:32:18 +0200

    Fix `InvoiceViewSet` view in `telegram_bots` app (#224)

--- telegram_bots/views/invoice.py
+++ telegram_bots/views/invoice.py
@@ -1,10 +1,12 @@
 from django.db.models import QuerySet
 
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin
+from rest_framework.parsers import JSONParser
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
 
 from constructor_telegram_bots.mixins import IDLookupMixin
+from constructor_telegram_bots.parsers import MultiPartJSONParser
 from constructor_telegram_bots.permissions import ReadOnly
 from users.authentication import JWTAuthentication
 from users.permissions import IsTermsAccepted
@@ -17,6 +19,7 @@ from .mixins import TelegramBotMixin
 class InvoiceViewSet(IDLookupMixin, TelegramBotMixin, ModelViewSet[Invoice]):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated & (IsTermsAccepted | ReadOnly)]
+    parser_classes = [JSONParser, MultiPartJSONParser]
     serializer_class = InvoiceSerializer
 
     def get_queryset(self) -> QuerySet[Invoice]:
