429365cd6ded9bbfae001712c278d2c2af818981
exg1o <77574092+EXG1O@users.noreply.github.com>
2026-01-04 23:55:41 +0200

    Update `MultiPartJSONParser` parser (#223)

--- constructor_telegram_bots/parsers.py
+++ constructor_telegram_bots/parsers.py
@@ -1,67 +1,66 @@
 from django.core.files.uploadedfile import UploadedFile
+from django.http import QueryDict
 from django.utils.datastructures import MultiValueDict
 from django.utils.translation import gettext_lazy as _
 
 from rest_framework.exceptions import ParseError
 from rest_framework.parsers import DataAndFiles, MultiPartParser
 
-from itertools import chain
 from json import JSONDecodeError
 from typing import Any
 import json
 import re
 
+MEDIA_DATA_FIELD_PATTERN: re.Pattern[str] = re.compile(
+    r'^(?P<name>\w+)(:(?P<index>\d+))?:data$', re.IGNORECASE
+)
+
 
 class MultiPartJSONParser(MultiPartParser):
-    """Parser for JSON data in multipart form data with support for files and their extra data."""
+    """Parser for JSON data in multipart form data with support for files and their data."""
 
-    error_detail = _("Не удалось проанализировать JSON данные для ключа '%(key)s'.")
-
-    def parse_json(self, key: str, data: str) -> dict[str, Any]:
+    def parse_json(self, field: str, value: str) -> dict[str, Any]:
         try:
-            return json.loads(data)
+            return json.loads(value)
         except JSONDecodeError as error:
-            raise ParseError(self.error_detail % {'key': key}) from error
+            raise ParseError(
+                _("Не удалось проанализировать JSON данные для поля '%(field)s'.")
+                % {'field': field}
+            ) from error
 
     def parse(self, *args: Any, **kwargs: Any) -> dict[str, Any]:  # type: ignore [override]
-        parsed: DataAndFiles[dict[str, Any], MultiValueDict[str, UploadedFile]] = (
+        """Parses the incoming bytestream as a multipart encoded form, and returns a dict object."""
+
+        multipart: DataAndFiles[QueryDict, MultiValueDict[str, UploadedFile]] = (
             super().parse(*args, **kwargs)  # type: ignore [assignment]
         )
 
-        data: dict[str, Any] = (
+        result: dict[str, Any] = (
             self.parse_json('data', raw_data)
-            if (raw_data := parsed.data.get('data'))
+            if (raw_data := multipart.data.get('data'))
             else {}
         )
 
-        for key, value in chain(parsed.data.items(), parsed.files.items()):
-            result: re.Match[str] | None = re.fullmatch(
-                r'^(?P<name>\w+):(?P<index>\d+)$', key, re.IGNORECASE
-            )
-
-            if not result:
+        for key, value in multipart.data.items():
+            if not isinstance(value, str):
                 continue
 
-            name, index = result.group('name', 'index')
-            name_plural: str = f'{name}s'
+            match: re.Match[str] | None = MEDIA_DATA_FIELD_PATTERN.fullmatch(key)
 
-            extra_data_key: str = f'{name}:{index}:extra_data'
-            extra_data: dict[str, Any] = (
-                self.parse_json(extra_data_key, raw_extra_data)
-                if (raw_extra_data := parsed.data.get(extra_data_key))
-                else {}
+            if not match:
+                continue
+
+            name, index = match.group('name', 'index')
+
+            media_data: dict[str, Any] = self.parse_json(key, value)
+            media_data['file'] = multipart.files.get(
+                f'{name}:{index}' if index else name
             )
 
-            items: list[dict[str, Any]] = data.setdefault(name_plural, [])
+            if index:
+                media: list[dict[str, Any]] = result.setdefault(name, [])
+                media.append(media_data)
+            else:
+                result[name] = media_data
 
-            if isinstance(value, UploadedFile):
-                items.append({'file': value, **extra_data})
-            elif isinstance(value, str):
-                if value.isdigit():
-                    items.append({'id': int(value), **extra_data})
-                else:
-                    items.append({'url': value, **extra_data})
-            elif len(items) < 1:
-                del data[name_plural]
-
-        return data
+        return result
