77a15879fd2d0fa0026718788a31367f1123a2cb
exg1o <exg1o.me@gmail.com>
2026-03-18 05:04:18 +0200

    Update `TemporaryVariableSerializer` serializer in `telegram_bots/hub` app

--- telegram_bots/hub/serializers/temporary_variable.py
+++ telegram_bots/hub/serializers/temporary_variable.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
 from ...models import TemporaryVariable
+from .connection import ConnectionSerializer
 
 
 class TemporaryVariableSerializer(serializers.ModelSerializer[TemporaryVariable]):
+    source_connections = ConnectionSerializer(many=True)
+
     class Meta:
         model = TemporaryVariable
-        fields = ['id', 'name', 'value']
+        fields = ['id', 'name', 'value', 'source_connections']
