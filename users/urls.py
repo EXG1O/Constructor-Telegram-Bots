5a235a5a013f2e708b4d652ad9ccddaf1116813d
exg1o <exg1o.me@gmail.com>
2026-03-26 17:22:56 +0200

    Fix urls in `users` app

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path
 
+from rest_framework.routers import SimpleRouter
+
 from .routers import UserRouter
 from .views import StatsAPIView, TokenViewSet, UserViewSet
 
 user_router = UserRouter(use_regex_path=False)
 user_router.register('', UserViewSet, basename='user')
-user_router.register('tokens', TokenViewSet, basename='token')
 
+default_router = SimpleRouter(use_regex_path=False)
+default_router.register('tokens', TokenViewSet, basename='token')
 
 app_name = 'users'
-urlpatterns = [path('stats/', StatsAPIView.as_view(), name='stats')] + user_router.urls
+urlpatterns = (
+    [path('stats/', StatsAPIView.as_view(), name='stats')]
+    + user_router.urls
+    + default_router.urls
+)
